88950cdd6d1cc5be391ffdc82f4819c787e60b98
Sina Moradbakhti <sina.moradbakhti@gmail.com>
2025-01-29 20:15:30 +0100

    projects card updated, archive projects + endpoints + services + screen and controller are updated

--- lib/Features/archived_projcts/domain/archived_projcts_repo.dart
+++ lib/Features/archived_projcts/domain/archived_projcts_repo.dart
@@ -4,4 +4,6 @@ import 'package:idealize_new_version/Core/Data/Models/project_model.dart';
 
 abstract class ArchivedProjctsRepository {
   Future<List<Project>> fetchAll();
+  Future<String?> archive({required String projectId});
+  Future<bool> unarchive({required String archiveId});
 }
