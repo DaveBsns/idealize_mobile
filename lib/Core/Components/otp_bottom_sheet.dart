e11c68dbe7e9efeb47c71eb1580acfdb46680866
Sina Moradbakhti <sina.moradbakhti@gmail.com>
2025-02-18 23:03:21 +0100

    otp refactored

--- lib/Core/Components/otp_bottom_sheet.dart
+++ lib/Core/Components/otp_bottom_sheet.dart
@@ -5,18 +5,10 @@ import 'package:idealize_new_version/Core/Components/buttons_widget.dart';
 import 'dart:async';
 import 'package:idealize_new_version/Core/I18n/messages.dart';
 import 'package:idealize_new_version/Core/Constants/config.dart';
-import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
-import 'package:idealize_new_version/Core/Components/buttons_widget.dart';
-import 'package:idealize_new_version/Core/Components/textfields_widget.dart';
-import 'package:idealize_new_version/Core/Constants/config.dart';
-import 'package:idealize_new_version/Core/Utils/extensions.dart';
-import 'package:idealize_new_version/app_repo.dart';
-import 'package:get/get.dart';
-import 'package:idealize_new_version/Core/I18n/messages.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class OtpController extends GetxController {
-  var code = List.filled(4, '').obs;
+  var code = ''.obs;
   var timeLeft = 120.obs;
   Timer? _timer;
 
@@ -35,36 +27,6 @@ class OtpController extends GetxController {
         timer.cancel();
       }
     });
-  }
-
-  void onDigitEntered(int index, String value) {
-    if (value.isNotEmpty &&
-        value.length == 1 &&
-        value.contains(RegExp(r'^\d$'))) {
-      code[index] = value;
-      if (index < 3) {
-        Get.focusScope?.nextFocus();
-      }
-    } else if (value.isEmpty) {
-      code[index] = '';
-      if (index > 0) {
-        Get.focusScope?.previousFocus();
-      }
-    } else {
-      code[index] = '';
-    }
-  }
-
-  bool isCodeComplete() {
-    return code.every((digit) => digit.isNotEmpty);
-  }
-
-  bool isButtonEnabled() {
-    return isCodeComplete() && timeLeft.value > 0;
-  }
-
-  String getCode() {
-    return code.join();
   }
 
   @override
@@ -89,114 +51,115 @@ class OtpBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final OtpController controller = Get.put(OtpController());
-    return Obx(() => Container(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text('Enter 4-digit Code',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-              Gap(AppConfig().dimens.medium),
-              Text("Enter the 4-digit code that you received via email.",
-                  style: TextStyle(
+    return Obx(
+      () => Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(AppStrings.enterVerificationCode.tr,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+            Gap(AppConfig().dimens.medium),
+            Text(AppStrings.enterVerificationCodeHint.tr,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: AppConfig().colors.lightGrayColor,
+                ),
+                textAlign: TextAlign.center),
+            Gap(AppConfig().dimens.extraLarge),
+            PinCodeTextField(
+              appContext: context,
+              keyboardType: TextInputType.number,
+              length: 4,
+              onChanged: (newCode) {
+                controller.code.value = newCode;
+              },
+              pinTheme: PinTheme(
+                borderRadius: BorderRadius.circular(4),
+                borderWidth: 1,
+                selectedColor: AppConfig().colors.primaryColor,
+                selectedFillColor: Colors.white,
+                inactiveColor: AppConfig().colors.lightGrayColor,
+                inactiveFillColor: Colors.white,
+                activeColor: AppConfig().colors.primaryColor,
+                activeFillColor: Colors.white,
+                disabledColor: AppConfig().colors.lightGrayColor,
+                shape: PinCodeFieldShape.box,
+                fieldWidth: 50,
+                fieldHeight: 50,
+                fieldOuterPadding: EdgeInsets.zero,
+              ),
+            ).paddingSymmetric(horizontal: 30),
+            Gap(AppConfig().dimens.large),
+            Text(
+                AppStrings.timeLeft.trParams(
+                  {
+                    'time': controller.timeLeft.value.toString(),
+                  },
+                ),
+                style: TextStyle(
+                    color: AppConfig().colors.txtColor,
                     fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: AppConfig().colors.lightGrayColor,
-                  ),
-                  textAlign: TextAlign.center),
-              Gap(AppConfig().dimens.extraLarge),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: List.generate(4, (index) {
-                  return SizedBox(
-                    width: 50,
-                    child: TextField(
-                      controller:
-                          TextEditingController(text: controller.code[index]),
-                      onChanged: (value) =>
-                          controller.onDigitEntered(index, value),
-                      maxLength: 1,
-                      keyboardType: TextInputType.number,
-                      textAlign: TextAlign.center,
-                      cursorColor: AppConfig().colors.primaryColor,
-                      decoration: InputDecoration(
-                        counterText: '',
-                        border: const OutlineInputBorder(),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: AppConfig().colors.lightGrayColor),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: AppConfig().colors.primaryColor,
-                              width: 1.4),
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-              ).paddingSymmetric(horizontal: 30),
-              Gap(AppConfig().dimens.large),
-              Obx(() => Text('Time left: ${controller.timeLeft.value}s',
-                  style: TextStyle(
-                      color: AppConfig().colors.txtColor,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500))),
-              const Gap(85),
-              Obx(() => CustomIconButton(
-                    title: "Weiter",
-                    onTap: controller.isButtonEnabled()
-                        ? () {
-                            String code = controller.getCode();
+                    fontWeight: FontWeight.w500)),
+            const Gap(85),
+            CustomIconButton(
+              title: AppStrings.next.tr,
+              onTap:
+                  controller.code.value.length == 4 && controller.timeLeft > 0
+                      ? () {
+                          String code = controller.code.value;
 
-                            onCodeSubmitted(code);
-                          }
-                        : null,
-                    txtColor: AppConfig().colors.primaryColor,
-                  )),
-              TextButton(
-                onPressed: () {
-                  controller.startTimer();
-                  onTappedResendCode();
-                },
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    style: TextStyle(color: AppConfig().colors.txtColor),
-                    children: <TextSpan>[
-                      TextSpan(text: AppStrings.didntReceiveCode.tr + "  "),
-                      TextSpan(
-                        text: AppStrings.resendCode.tr,
-                        style: TextStyle(
-                            color: AppConfig().colors.secondaryColor,
-                            fontWeight: FontWeight.w500,
-                            decoration: TextDecoration.underline),
-                      ),
-                    ],
-                  ),
+                          onCodeSubmitted(code);
+                        }
+                      : null,
+              txtColor: AppConfig().colors.primaryColor,
+            ),
+            TextButton(
+              onPressed: () {
+                controller.startTimer();
+                onTappedResendCode();
+              },
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: TextStyle(color: AppConfig().colors.txtColor),
+                  children: <TextSpan>[
+                    TextSpan(text: AppStrings.didntReceiveCode.tr + "  "),
+                    TextSpan(
+                      text: AppStrings.resendCode.tr,
+                      style: TextStyle(
+                          color: AppConfig().colors.secondaryColor,
+                          fontWeight: FontWeight.w500,
+                          decoration: TextDecoration.underline),
+                    ),
+                  ],
                 ),
               ),
-              TextButton(
-                onPressed: () => onTappedEditInformation(),
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    style: TextStyle(color: AppConfig().colors.txtColor),
-                    children: <TextSpan>[
-                      TextSpan(text: AppStrings.editInformation.tr + "  "),
-                      TextSpan(
-                        text: AppStrings.edit.tr,
-                        style: TextStyle(
-                            color: AppConfig().colors.secondaryColor,
-                            fontWeight: FontWeight.w500,
-                            decoration: TextDecoration.underline),
-                      ),
-                    ],
-                  ),
+            ),
+            TextButton(
+              onPressed: () => onTappedEditInformation(),
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: TextStyle(color: AppConfig().colors.txtColor),
+                  children: <TextSpan>[
+                    TextSpan(text: AppStrings.editInformation.tr + "  "),
+                    TextSpan(
+                      text: AppStrings.edit.tr,
+                      style: TextStyle(
+                          color: AppConfig().colors.secondaryColor,
+                          fontWeight: FontWeight.w500,
+                          decoration: TextDecoration.underline),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
